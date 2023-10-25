eb8b5d72c2fd2289f92a168e1698caa3c8f7e28b
horinokosuke <kosuke19990808@icloud.com>
2023-10-25 20:27:52 +0900

    SUDSの表示

--- ExperimentSystem_VRET/Assets/Script/PresenSystem.cs
+++ ExperimentSystem_VRET/Assets/Script/PresenSystem.cs
@@ -14,6 +14,8 @@ public class PresenSystem : MonoBehaviour
     private int nowThemeCount;//現在のプレゼン数
     private int limitTime; //制限時間　
     public int timeSpan;　//プレゼン時間設定
+    private bool isSUDS;
+    private bool isRestart;
 
     public string theme1;
     public string theme2;
@@ -28,6 +30,7 @@ public class PresenSystem : MonoBehaviour
     private List<string> themeList; //プレゼンテーマリスト
 
     internal bool isPresentating;
+    internal int status;
 
     internal int second; //カウントダウンの時間（秒）
 
@@ -37,20 +40,53 @@ public class PresenSystem : MonoBehaviour
     {
         second = -1;
         nowThemeCount = 0;
-        limitTime = timeSpan;
+        limitTime = 0;
         themeList = new List<string>() { theme1, theme2, theme3, theme4, theme5, theme6, theme7, theme8, theme9 };
         presenMonitor.SetText(theme1);
-        isPresentating = true;
+        isSUDS = true;
+        isRestart = false;
+        status = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isPresentating)
+
+        if(status == 0)
         {
+            presenMonitor.SetText("Spaceでスタート");
+            if (Input.GetKeyDown("space"))
+            {
+                status = 1;
+            }
+        }
+        else if (status == 1)
+        {
+            if (isSUDS)
+            {
+                if (Input.GetKeyDown("space"))
+                {
+                    isSUDS = false;
+                    isRestart = true;
+                }
+                ShowSUDS();
+            }
+            else if (isRestart)
+            {
+                RestartTime();
+                ShowTheme();
+                ShowLimitTime();
+            }
+            else
+            {
+                ChangeTheme();
+                ShowLimitTime();
+            }
             LimitTimer();
-            ShowLimitTime();
-            ShowTheme();
+        }
+        else if(status == 2)
+        {
+
         }
     }
 
@@ -60,6 +96,14 @@ public class PresenSystem : MonoBehaviour
     internal void LimitTimer()
     {
         second = limitTime - (int)Math.Round(Time.time);
+    }
+
+    /// <summary>
+    /// 時間を再開
+    /// </summary>
+    internal void RestartTime()
+    {
+        limitTime = (int)Math.Round(Time.time);
     }
 
     /// <summary>
@@ -75,28 +119,53 @@ public class PresenSystem : MonoBehaviour
     /// <summary>
     /// プレゼンテーマの表示
     /// </summary>
-    internal void ShowTheme()
+    internal void ChangeTheme()
     {
         if (second > 0)
         {
             return;
         }
 
-        nowThemeCount++;
-        limitTime += timeSpan;
+        if (nowThemeCount == 0 || nowThemeCount == 3 || nowThemeCount == 6 || nowThemeCount == 9)
+        {
+            isSUDS = true;
+            return;
+        }
+        
+        ShowTheme();
+    }
+    internal void ShowTheme()
+    {
+        if (second > 0)
+        {
+            return;
+        }
+        limitTime += timeSpan; //制限時間をセット
 
         for (int count = 0; count < themeList.Count; count++)
         {
             if (count == nowThemeCount)
             {
                 int _num = count + 1;
-                presenMonitor.SetText(_num + themeList[count]);
+                presenMonitor.SetText(_num + "\r\n" + themeList[count]);
             }
             else if (nowThemeCount >= themeNum) //現在のテーマ数が全テーマ数を超えたとき
             {
-                isPresentating = false; //プレゼンを終了
-                presenMonitor.SetText("終了");
+                status = 2;
+                presenMonitor.SetText("お疲れ様でした。\r\n ヘッドセットを外して実験者に声をかけてください"); //プレゼンを終了
+                second = 0;
             }
         }
+        nowThemeCount++;
+        isRestart = false;
     }
+
+    /// <summary>
+    /// SUDSの表示
+    /// </summary>
+    internal void ShowSUDS()
+    {
+        presenMonitor.SetText("今の不安について1～100で答えてください");
+    }
+
 }
